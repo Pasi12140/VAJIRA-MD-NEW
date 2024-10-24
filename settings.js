7cee396d7bb2a680e30032f323f4c451ace7dae7
Pasi12140 <Pasibot10@gmail.com>
2024-10-24 17:10:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=DUtz0LLa#aVJgJ2Q5KYMSUpvO7hpvRzWuLvBuvbpYkm5MO-TehzM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=SY0kyJwY#dkoZvg82XiO2mdAqejvyjPe5U9KvyUIzq0ChzIk6dgI' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94725029033' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
